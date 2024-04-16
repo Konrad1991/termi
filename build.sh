fbd515c684f345fccebb3cf3a52f4e76703c6340
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-16 16:27:04 +0200

    added run fct

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ ./buildTest.cpp -std=c++20
--- lua/termi/init.lua
+++ lua/termi/init.lua
@@ -26,11 +26,22 @@ function run_build()
 	local build_script = vim.fn.findfile("build.sh", current_dir)
 	if build_script ~= "" then
 		local output = {}
-		local handle = io.popen("cd " .. current_dir .. " && ./build.sh 2>&1")
-		for line in handle:lines() do
-			table.insert(output, line)
-		end
-		handle:close()
+		local handle = vim.loop.spawn("sh", {
+			args = { "-c", "cd " .. current_dir .. " && ./build.sh" },
+			stdio = { nil, true, 2 },
+		}, function(code, _)
+			if code == 0 then
+				handle:close()
+			end
+		end)
+		handle:read_start(function(err, data)
+			if err then
+				return
+			end
+			if data then
+				table.insert(output, data)
+			end
+		end)
 		return output
 	else
 		return { "build.sh not found in the current directory." }
