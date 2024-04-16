f82a4830e06960356add083890424f006f661553
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-16 15:47:05 +0200

    added search for build script fct

--- lua/termi/init.lua
+++ lua/termi/init.lua
@@ -1,7 +1,17 @@
 local M = {}
 
 function M.hello()
-  print("Hello, world!")
+	print("Hello, world!")
+end
+
+function M.find_build_script()
+	local current_dir = vim.fn.expand("%:p:h")
+	local build_script = vim.fn.findfile("build.sh", current_dir)
+	if build_script ~= "" then
+		print("Found build.sh:", build_script)
+	else
+		print("build.sh not found in the current directory.")
+	end
 end
 
 return M
