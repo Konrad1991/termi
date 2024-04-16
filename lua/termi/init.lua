fd3d36294a30e077a4db17936a764ef52ee9fd20
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-16 16:42:58 +0200

    added run fct & tests

--- lua/termi/init.lua
+++ lua/termi/init.lua
@@ -1,47 +1,10 @@
 local M = {}
 
-function M.run_build()
-	local current_dir = vim.fn.expand("%:p:h")
-	local build_script = vim.fn.findfile("build.sh", current_dir)
-	if build_script ~= "" then
-		vim.cmd("terminal cd " .. current_dir .. " && ./build.sh")
-	else
-		print("build.sh not found in the current directory.")
-	end
-end
-
--- function run_build()
--- 	local current_dir = vim.fn.expand("%:p:h")
--- 	local build_script = vim.fn.findfile("build.sh", current_dir)
--- 	if build_script ~= "" then
--- 		local output = vim.fn.systemlist("cd " .. current_dir .. " && ./build.sh")
--- 		return output
--- else
--- return { "build.sh not found in the current directory." }
--- end
--- end
-
 function run_build()
 	local current_dir = vim.fn.expand("%:p:h")
 	local build_script = vim.fn.findfile("build.sh", current_dir)
 	if build_script ~= "" then
-		local output = {}
-		local handle = vim.loop.spawn("sh", {
-			args = { "-c", "cd " .. current_dir .. " && ./build.sh" },
-			stdio = { nil, true, 2 },
-		}, function(code, _)
-			if code == 0 then
-				handle:close()
-			end
-		end)
-		handle:read_start(function(err, data)
-			if err then
-				return
-			end
-			if data then
-				table.insert(output, data)
-			end
-		end)
+		local output = vim.fn.systemlist("cd " .. current_dir .. " && ./build.sh")
 		return output
 	else
 		return { "build.sh not found in the current directory." }
