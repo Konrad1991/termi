bbd887abb5cfbb119671163e830a9811d582e389
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-16 15:50:23 +0200

    run build fct

--- lua/termi/init.lua
+++ lua/termi/init.lua
@@ -1,14 +1,10 @@
 local M = {}
 
-function M.hello()
-	print("Hello, world!")
-end
-
-function M.find_build_script()
+function M.run_build()
 	local current_dir = vim.fn.expand("%:p:h")
 	local build_script = vim.fn.findfile("build.sh", current_dir)
 	if build_script ~= "" then
-		print("Found build.sh:", build_script)
+		vim.cmd("terminal cd " .. current_dir .. " && ./build.sh")
 	else
 		print("build.sh not found in the current directory.")
 	end
